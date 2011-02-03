60e6c1da43b1c715136d6d829f57ebc53dc2ad39
makamaka <makamaka.donzoko@gmail.com>
2011-02-03 15:35:24 +0900

    added constructor method and spec_version

--- lib/Geo/Hex.pm
+++ lib/Geo/Hex.pm
@@ -12,12 +12,60 @@ our @ISA    = qw(Exporter);
 our @EXPORT    = qw(decode_geohex encode_geohex);
 our @EXPORT_OK = qw(latlng2geohex geohex2latlng getZoneByLocation getZoneByCode);
 
+my $Default_SPEC = 3;
 
 sub import {
 
 }
 
 
+sub new {
+    my ( $class, @args ) = @_;
+
+    ref $class && Carp::croak "$class is not a GeoHex class name.";
+
+    my %args = @_ == 2 ? %{ $args[0] } : @args;
+    my $v = $args{ version } || $args{ v } || $Default_SPEC;
+
+    $v =~ /^[123]$/ or Carp::croak "The GeoHex spec version is allowed to 1 to 3";
+
+    my $pkg = $class . $v . '::Coder';
+
+    return bless { v => $v }, $pkg;
+}
+
+
+sub spec_version { return $Default_SPEC; }
+
+
+#
+#
+#
+
+package
+    Geo::Hex::Coder;
+
+sub spec_version {
+    $_[0]->{v};
+}
+
+
+package
+    Geo::Hex3::Coder;
+
+our @ISA = qw(Geo::Hex::Coder);
+
+
+package
+    Geo::Hex2::Coder;
+
+our @ISA = qw(Geo::Hex::Coder);
+
+
+package
+    Geo::Hex1::Coder;
+
+our @ISA = qw(Geo::Hex::Coder);
 
 
 1;
@@ -46,6 +94,9 @@ Geo::Hex - GeoHex decoder/encoder
     # $zone->x     : Mercator X coordinate of given GeoHex's center point 
     # $zone->y     : Mercator Y coordinate of given GeoHex's center point 
     
+    $geohex->spec_version; # => 3
+    
+    
     # Export function - GeoHex v3 by default
     $zone = decode_geohex( 'XM4885487' );
     $code = encode_geohex( $zone );
@@ -60,31 +111,76 @@ Geo::Hex - GeoHex decoder/encoder
     my ( $center_lat, $center_lng, $level ) = geohex2latlng( $code );
     
     
-    # Explicit export function - Javascript original method
+    # Explicit export function - original javascript name
     use Geo::Hex v => 3, qw(getZoneByLocation getZoneByCode);
     # From latitude/longitude to zone object*
     $zone = getZoneByLocation( $lat, $lng, $level );
     # From hex code to zone object*
     $zone = getZoneByCode( $code );
 
-=head1 EXPORT
+=head1 CLASS METHODS
 
-=over
+=head2 new
 
-=item C<< latlng2geohex( $lat, $lng, $level ) >>
+Creates a new Geo::Hex::Coder object.
+
+=over 4
+
+=item version
+
+The GeoHex specification vresion.
+
+=item v
+
+The synonym to C<version> option.
+
+=back
+
+=head2 spec_version
+
+
+=head1 INSTANCE METHODS
+
+=head2 decode
+
+=head2 encode
+
+=head2 spec_version
+
+
+=head1 DEFAULT FUNCTIONS
+
+=head2 decode_geohex
+
+=head2 encode_geohex
+
+
+=head1 EXPORT FUNCTIONS
+
+=head2 latlng2geohex
+
+    latlng2geohex( $lat, $lng, $level );
+    
+    latlng2geohex( $zone );
 
 Convert latitude/longitude to GeoHex code.
 $level is optional, and default value is 16.
 
-=item C<< geohex2latlng( $hex ) >>
+=head2 geohex2latlng
+
+    geohex2latlng( $code );
 
 Convert GeoHex code to center latitude/longitude, and level value.
 
-=back
 
 =head1 SEE ALSO
 
 L<http://geogames.net/geohex/v3>
+
+L<Geo::Hex::Zone>, 
+L<Geo::Hex1>,
+L<Geo::Hex2>,
+L<Geo::Hex3>
 
 =head1 AUTHOR
 
--- t/000_load.t
+++ t/000_load.t
@@ -1,7 +1,7 @@
 #!perl
 
 use strict;
-use Test::More tests => 1;
+use Test::More;
 
 BEGIN {
     use_ok( 'Geo::Hex' );
@@ -9,3 +9,24 @@ BEGIN {
 
 diag( "Testing Geo::Hex $Geo::Hex::VERSION" );
 
+is( Geo::Hex->spec_version, 3, 'default spec version' );
+
+my $geohex;
+
+$geohex = Geo::Hex->new();
+isa_ok( $geohex, 'Geo::Hex3::Coder' );
+is( $geohex->spec_version, 3, 'spec version' );
+
+$geohex = Geo::Hex->new( version => 1 );
+isa_ok( $geohex, 'Geo::Hex1::Coder' );
+is( $geohex->spec_version, 1, 'spec version' );
+
+$geohex = Geo::Hex->new( v => 2 );
+isa_ok( $geohex, 'Geo::Hex2::Coder' );
+is( $geohex->spec_version, 2, 'spec version' );
+
+$geohex = Geo::Hex->new( v => 3 );
+isa_ok( $geohex, 'Geo::Hex3::Coder' );
+is( $geohex->spec_version, 3, 'spec version' );
+
+done_testing;
